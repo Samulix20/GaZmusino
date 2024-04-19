9ebdd2319a74613afde09fa4c0d4737cd99a03d8
Samulix20 <samulix2000@gmail.com>
2024-04-19 23:13:11 +0200

    decoder back to its own file

--- rtl/modules/decode_stage/rv32_decode_stage.sv
+++ rtl/modules/decode_stage/rv32_decode_stage.sv
@@ -17,6 +17,12 @@ module rv32_decode_stage (
 );
 
 decoded_buffer_data_t internal_data /*verilator public*/;
+decoded_instr_t decoder_output;
+
+rv32_decoder decoder(
+    .instr(instr_data.instr),
+    .decoded_instr(decoder_output)
+);
 
 // Decode logic
 always_comb begin
@@ -30,94 +36,8 @@ always_comb begin
     internal_data.reg1 = reg1;
     internal_data.reg2 = reg2;
 
-    // Default signals NOP Setup add x0, x0, 0;
-    internal_data.decoded_instr = create_nop_ctrl();
-
-    case(instr_data.instr.opcode)
-        // Load upper imm
-        // ALU: 0 + U_IMM
-        // RD = ALU
-        OPCODE_LUI: begin
-            internal_data.decoded_instr.t = INSTR_U_TYPE;
-            internal_data.decoded_instr.int_alu_i2 = ALU_IN_IMM;
-            internal_data.decoded_instr.register_wb = 1;
-        end
-
-        // Load upper imm+pc
-        // ALU: PC + U_IMM
-        // RD = ALU
-        OPCODE_AUIPC: begin
-            internal_data.decoded_instr.t = INSTR_U_TYPE;
-            internal_data.decoded_instr.int_alu_i1 = ALU_IN_PC;
-            internal_data.decoded_instr.int_alu_i2 = ALU_IN_IMM;
-            internal_data.decoded_instr.register_wb = 1;
-        end
-
-        // Jump and link
-        // ALU: PC + J_IMM
-        // PC = ALU
-        // RD = PC + 4
-        OPCODE_JAL: begin
-            internal_data.decoded_instr.t = INSTR_J_TYPE;
-            internal_data.decoded_instr.branch_op = OP_J;
-            internal_data.decoded_instr.int_alu_i1 = ALU_IN_PC;
-            internal_data.decoded_instr.int_alu_i2 = ALU_IN_IMM;
-            internal_data.decoded_instr.register_wb = 1;
-            internal_data.decoded_instr.wb_result_src = WB_PC4;
-        end
-
-        // Jump and link using register
-        // ALU: R1 + J_IMM
-        // PC = ALU
-        // RD = PC + 4
-        OPCODE_JALR: begin
-            internal_data.decoded_instr.t = INSTR_R_TYPE;
-            internal_data.decoded_instr.branch_op = OP_J;
-            internal_data.decoded_instr.int_alu_i1 = ALU_IN_REG_1;
-            internal_data.decoded_instr.int_alu_i2 = ALU_IN_IMM;
-            internal_data.decoded_instr.register_wb = 1;
-            internal_data.decoded_instr.wb_result_src = WB_PC4;
-        end
-
-        // Branch instruction
-        // ALU: PC + B_IMM
-        // PC = ALU
-        // B_UNIT: R1, R2
-        OPCODE_BRANCH: begin
-            internal_data.decoded_instr.t = INSTR_B_TYPE;
-            internal_data.decoded_instr.branch_op = branch_op_t'({1'b0, instr_data.instr.funct3});
-            internal_data.decoded_instr.int_alu_i1 = ALU_IN_PC;
-            internal_data.decoded_instr.int_alu_i2 = ALU_IN_IMM;
-        end
-
-        // Integer Immediate arithmetic
-        // ALU: R1, I_IMM
-        // RD = ALU
-        OPCODE_INTEGER_IMM: begin
-            internal_data.decoded_instr.t = INSTR_I_TYPE;
-            internal_data.decoded_instr.int_alu_op = int_alu_op_t'({1'b0, instr_data.instr.funct3});
-            internal_data.decoded_instr.int_alu_i1 = ALU_IN_REG_1;
-            internal_data.decoded_instr.int_alu_i2 = ALU_IN_IMM;
-            internal_data.decoded_instr.register_wb = 1;
-        end
-
-        // Integer register arithmetic
-        // ALU: R1, R2
-        // RD = ALU
-        OPCODE_INTEGER_REG: begin
-            internal_data.decoded_instr.t = INSTR_R_TYPE;
-            internal_data.decoded_instr.int_alu_op =
-                int_alu_op_t'({instr_data.instr.funct7[5], instr_data.instr.funct3});
-            internal_data.decoded_instr.int_alu_i1 = ALU_IN_REG_1;
-            internal_data.decoded_instr.int_alu_i2 = ALU_IN_REG_2;
-            internal_data.decoded_instr.register_wb = 1;
-        end
-
-        default: begin
-            // Invalid instruction detection
-            internal_data.decoded_instr.invalid = 1;
-        end
-    endcase
+    // Default decode
+    internal_data.decoded_instr = decoder_output;
 
     if(set_nop | !resetn) begin
         internal_data.instr = `RV_NOP;
