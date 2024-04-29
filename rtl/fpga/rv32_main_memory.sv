280f4ce52cc34ca31c07194b5ebb3e91da775171
Samulix20 <samulix2000@gmail.com>
2024-04-29 12:01:04 +0200

    fixing memory bugs

--- rtl/fpga/rv32_main_memory.sv
+++ rtl/fpga/rv32_main_memory.sv
@@ -13,52 +13,74 @@ module rv32_main_memory (
 
 logic [31:0] instr;
 logic [31:0] data;
+
+logic [7:0] data_in_b [4];
 logic [3:0] we_b;
 
-bram b0(
+bram #(.NUM_BYTES(100)) b0(
     .clk(clk), .resetn(resetn),
     .addr_a(instr_request.addr), .addr_b(data_request.addr),
-    .data_in_b(data_request.data), .we_b(we_b[0]),
-    .data_a(data[7:0]), .data_b(instr[7:0])
+    .data_in_b(data_in_b[0]), .we_b(we_b[0]),
+    .data_a(instr[7:0]), .data_b(data[7:0])
 );
 
-bram b1(
+bram #(.NUM_BYTES(100)) b1(
     .clk(clk), .resetn(resetn),
     .addr_a(instr_request.addr), .addr_b(data_request.addr),
-    .data_in_b(data_request.data), .we_b(we_b[1]),
-    .data_a(data[15:8]), .data_b(instr[15:8])
+    .data_in_b(data_in_b[1]), .we_b(we_b[1]),
+    .data_a(instr[15:8]), .data_b(data[15:8])
 );
 
-bram b2(
+bram #(.NUM_BYTES(100)) b2(
     .clk(clk), .resetn(resetn),
     .addr_a(instr_request.addr), .addr_b(data_request.addr),
-    .data_in_b(data_request.data), .we_b(we_b[2]),
-    .data_a(data[23:16]), .data_b(instr[23:16])
+    .data_in_b(data_in_b[2]), .we_b(we_b[2]),
+    .data_a(instr[23:16]), .data_b(data[23:16])
 );
 
-bram b3(
+bram #(.NUM_BYTES(100)) b3(
     .clk(clk), .resetn(resetn),
     .addr_a(instr_request.addr), .addr_b(data_request.addr),
-    .data_in_b(data_request.data), .we_b(we_b[3]),
-    .data_a(data[31:24]), .data_b(instr[31:24])
+    .data_in_b(data_in_b[3]), .we_b(we_b[3]),
+    .data_a(instr[31:24]), .data_b(data[31:24])
 );
 
 always_comb begin
     we_b = 0;
+
+    // Default setup
+    data_in_b[0] = data_request.data[7:0];
+    data_in_b[1] = data_request.data[15:8];
+    data_in_b[2] = data_request.data[23:16];
+    data_in_b[3] = data_request.data[31:24];
+
     case(data_request.op)
         MEM_SB: begin
             case(data_request.addr[1:0])
                 2'b00: we_b = 4'b0001;
-                2'b01: we_b = 4'b0010;
-                2'b10: we_b = 4'b0100;
-                2'b11: we_b = 4'b1000;
+                2'b01: begin
+                    we_b = 4'b0010;
+                    data_in_b[1] = data_in_b[0];
+                end
+                2'b10: begin
+                    we_b = 4'b0100;
+                    data_in_b[2] = data_in_b[0];
+                end
+                2'b11: begin
+                    we_b = 4'b1000;
+                    data_in_b[3] = data_in_b[0];
+                end
                 default: we_b = 0; // Unreacheable
             endcase
         end
         MEM_SH: begin
             case(data_request.addr[1:0])
                 2'b00: we_b = 4'b0011;
-                2'b10: we_b = 4'b1100;
+                2'b10: begin
+                    we_b = 4'b1100;
+                    data_in_b[2] = data_in_b[0];
+                    data_in_b[3] = data_in_b[1];
+                end
                 default: we_b = 0; // Dont write aligment error
             endcase
         end
--- testbench/memory_tb.cpp
+++ testbench/memory_tb.cpp
@@ -7,6 +7,11 @@
 #include <verilated_vcd_c.h>
 
 #include "Vrv32_main_memory.h"
+#include "Vrv32_main_memory___024unit.h"
+
+using MemoryRequest = Vrv32_main_memory_memory_request_t__struct__0;
+using MemoryResponse = Vrv32_main_memory_memory_response_t__struct__0;
+using MemoryRV32 = Vrv32_main_memory___024unit;
 
 constexpr uint64_t max_sim_time = 200;
 
@@ -36,13 +41,30 @@ int main(int argc, char** argv) {
         bool reset_on = sim_time < 5;
         dut->resetn = static_cast<uint8_t>(!reset_on);
 
-        // Memory bus signals
+        
+        if(!reset_on) {
+
+            if(sim_time >= 5 && sim_time <= 6) {
+                MemoryRequest r0;
+                r0.addr = 4;
+                r0.data = 0xDEADBEEF;
+                r0.op = MemoryRV32::MEM_SW;
+                dut->data_request = r0.get();
+            }
+
+            else if (sim_time >= 7 && sim_time <= 8) {
+                MemoryRequest r0;
+                r0.addr = 4;
+                r0.data = 0;
+                r0.op = MemoryRV32::MEM_LW;
+                dut->data_request = r0.get();
+            }
+            
+        }
 
         // Update signals
         dut->eval();
 
-        // Debug
-    
         // Trace waveform
         m_trace->dump(sim_time);
         
