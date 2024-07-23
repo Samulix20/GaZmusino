2b4cba2975c7d527ed5453b4674c11900bfd4ba3
Samulix20 <samulix2000@gmail.com>
2024-07-23 10:53:22 +0200

    better test error msg + reorder of test files

--- test/test.sh
+++ test/test.sh
@@ -7,7 +7,7 @@ NC='\e[0m'
 # ISA TEST SECTION
 
 cd isa_tests
-rv_tests="$(ls base/base.S rv32ui/*.S) $(ls rv32um/*.S | grep -E mul)"
+rv_tests="$(ls base/base.S rv32ui/*.S rv32zicsr/*.S rv32mul/*.S)"
 cd ..
 
 i=1
@@ -15,39 +15,50 @@ num_tests=$(echo "$rv_tests" | wc -l)
 num_pass=0
 num_fail=0
 
-echo "Running $num_tests isa_tests..."
+echo "STARTING $num_tests ISA TESTS... "
 
 for test in $rv_tests
 do
-    make -s -C isa_tests TEST_TARGET=$test 2>/dev/null
+    # Build the test
+    build_output=$(make -C isa_tests TEST_TARGET=$test 2>&1)
     make_status=$?
 
+    # Check build result
     if [ $make_status -ne 0 ]; then
-        echo -e "$RED Error building test $NC"
-        exit "$make_status"
-    fi
-
-    test_res=$(../obj_dir/Vrv32_top +verilator+rand+reset+2\
-                -e ../build/isa_tests/${test%.S}.elf 2>&1)
-    test_status=$?
-
-    if [ $test_status -ne 0 ]; then
-        echo -e "$i/$num_tests\t $test\t ${RED}FAIL $test_status ${NC}"
-        echo -e "$test_res"
+        echo " "
+        echo -e "${RED}[$i] Error building test $test $NC"
+        echo -e "$build_output"
         num_fail=$((num_fail+1))
     else
-        num_pass=$((num_pass+1))
+        # Run simulation
+        test_res=$(../obj_dir/Vrv32_top +verilator+rand+reset+2\
+                    -e ../build/isa_tests/${test%.S}.elf 2>&1)
+        test_status=$?
+
+        # Check test result
+        if [ $test_status -ne 0 ]; then
+            echo -e " "
+            echo -e "${RED}$i/$num_tests\t $test\t FAIL $test_status ${NC}"
+            echo -e "$test_res"
+            num_fail=$((num_fail+1))
+        else
+            num_pass=$((num_pass+1))
+        fi
     fi
 
     i=$((i+1))
 done
 
+echo " "
+echo "RESULTS"
 if [ $num_fail -ne 0 ]; then
-    echo -e "${RED}$num_fail TEST FAILED${NC}"
-    echo -e "${GREEN}$num_pass TEST PASSED${NC}"
+    echo -e "${RED}[$num_fail/$num_tests] TEST FAILED${NC}"
+    echo -e "${GREEN}[$num_pass/$num_tests] TEST PASSED${NC}"
 else
-    echo -e "${GREEN}ALL TEST PASSED${NC}"
+    echo -e "${GREEN}ALL [$num_pass/$num_tests] TEST PASSED${NC}"
 fi
+
+echo " "
 
 # C TEST SECTION
 
@@ -61,7 +72,7 @@ num_tests=$(echo "$rv_tests" | wc -l)
 num_pass=0
 num_fail=0
 
-echo "Running $num_tests c_tests..."
+echo "STARTING $num_tests C TESTS... "
 
 for test in $rv_tests
 do
@@ -75,6 +86,7 @@ do
     test_status=$?
     
     if [ $test_status -ne 0 ]; then
+        echo " "
         echo -e "$i/$num_tests\t $test\t ${RED}FAIL $test_status ${NC}"
         echo -e "$test_res"
         num_fail=$((num_fail+1))
@@ -85,12 +97,16 @@ do
     i=$((i+1))
 done
 
+echo " "
+echo "RESULTS"
 if [ $num_fail -ne 0 ]; then
     echo -e "${RED}$num_fail TEST FAILED${NC}"
     echo -e "${GREEN}$num_pass TEST PASSED${NC}"
 else
-    echo -e "${GREEN}ALL TEST PASSED${NC}"
+    echo -e "${GREEN}ALL [$num_pass/$num_tests] TEST PASSED${NC}"
 fi
+
+echo " "
 
 # CPP TEST SECTION
 
@@ -102,7 +118,7 @@ num_tests=$(echo "$rv_tests" | wc -l)
 num_pass=0
 num_fail=0
 
-echo "Running $num_tests cpp_tests..."
+echo "STARTING $num_tests C++ TESTS... "
 
 for test in $rv_tests
 do
@@ -126,9 +142,13 @@ do
     i=$((i+1))
 done
 
+echo " "
+echo "RESULTS"
 if [ $num_fail -ne 0 ]; then
     echo -e "${RED}$num_fail TEST FAILED${NC}"
     echo -e "${GREEN}$num_pass TEST PASSED${NC}"
 else
-    echo -e "${GREEN}ALL TEST PASSED${NC}"
+    echo -e "${GREEN}ALL [$num_pass/$num_tests] TEST PASSED${NC}"
 fi
+
+echo " "
